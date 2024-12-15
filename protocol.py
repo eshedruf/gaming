458d75c63c48c086d9f76ec23c1b87c16ee5c2ff
noam <124458362+no4miko@users.noreply.github.com>
2024-12-15 15:57:44 +0200

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -4,8 +4,31 @@ import socket
 class Protocol:
     def __init__(self):
         self.SEPERATOR = "\r\n"
+        self.CMDS = ["SIGNUP", "LOGIN", "STOP", "CHECK", "GIVE_RANGE"]
+        self.LENGTH_FIELD_SIZE = 4
 
-    def sign_up(self, name, password, age):
-        msg = name + self.SEPERATOR + password + self.SEPERATOR + age
-        length = len(msg)
-        return length, msg
+    def check_cmd(self, cmd):
+        return cmd in self.CMDS
+
+    def create_msg(self, cmd, msg_parts):
+        if self.check_cmd(cmd):
+            msg = cmd
+            for part in msg_parts:
+                msg += self.SEPERATOR + part
+            return str(len(msg)) + msg
+        else:
+            raise FileExistsError
+
+    def get_msg(self, socket_gaming):
+        try:
+            data = socket_gaming.recv(self.LENGTH_FIELD_SIZE).decode()
+            length = int(data)
+            data_str = socket_gaming.recv(length)
+            while len(data_str) < length:
+                data_str += socket_gaming.recv(length - len(data_str))
+            return True, data_str.decode()
+        except ValueError:
+            return False, ""
+
+
+
