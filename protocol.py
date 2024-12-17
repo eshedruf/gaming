c328d36ff842fbec58cf3fb24f96da68b5fb1256
Fogopi33 <fogopi33@gmail.com>
2024-12-17 13:57:08 +0200

    ballz 9

--- protocol.py
+++ protocol.py
@@ -3,7 +3,7 @@ import socket
 
 class Protocol:
     def __init__(self):
-        self.SEPERATOR = "\r\n"
+        self.SEPERATOR = "\r"
         self.CMDS = ["SIGNUP", "LOGIN", "STOP", "CHECK", "GIVE_RANGE", "GIVE_MD5"]
         self.LENGTH_FIELD_SIZE = 4
         self.FOUND = "FOUND"
@@ -24,14 +24,16 @@ class Protocol:
     def get_msg(self, socket_gaming):
         try:
             data = socket_gaming.recv(self.LENGTH_FIELD_SIZE).decode()
+            print(data)
             length = int(data)
+            print(length)
             data_str = socket_gaming.recv(length)
             while len(data_str) < length:
                 data_str += socket_gaming.recv(length - len(data_str))
             msg = data_str.decode()
+            print(msg)
             command = msg.split(self.SEPERATOR)[0]
             lst = msg.split(self.SEPERATOR)[1::]
             return True, command, lst
         except ValueError:
             return False, "", ""
-
