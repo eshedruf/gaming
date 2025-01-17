1f06a9c229a3a8ba73112b892d241a13bd42959f
noam <124458362+no4miko@users.noreply.github.com>
2025-01-17 17:08:51 +0200

    Update client.py

--- client.py
+++ client.py
@@ -37,15 +37,13 @@ class Client:
         print("client connecting to server...")
         self.client_socket.connect((self.server_ip, self.server_port))
         is_valid, command, msg_parts = self.protocol.get_msg(self.client_socket)
+        print(123)
         if is_valid and command == self.protocol.CMDS[4]:
             self.server_public_key = msg_parts[0]
-            print(type(self.server_public_key))
-            print(self.server_public_key)
-            print(msg_parts[0])
             self.aes_key = get_random_bytes(self.protocol.KEY_LENGTH)
             rsa_cipher = self.protocol.get_rsa_cipher(self.server_public_key)
-            #encrypted_aes_key = self.protocol.rsa_encrypt_message(self.aes_key, rsa_cipher)
             msg = self.protocol.create_msg(self.protocol.CMDS[4], [self.aes_key], rsa_cipher)
+            print(msg)
             self.client_socket.send(msg)
             self.cipher_for_encryption = self.protocol.get_aes_cipher(self.aes_key)
             self.cipher_for_decryption = self.protocol.get_aes_cipher(self.aes_key)
@@ -57,15 +55,11 @@ class Client:
                 username = self.window.username_val
                 password = self.window.password_val
                 age = self.window.age_val
-                print(username)
-                print(password)
-                print(age)
 
                 if age != "-1":
                     # sign up
                     self.client_socket.send(self.protocol.create_msg("SIGNUP", [username, password, age], self.cipher_for_encryption))
                     is_valid = self.protocol.get_msg(self.client_socket, self.cipher_for_decryption)[0]
-                    print(is_valid)
                     if is_valid:
                         self.window.clear_screen()
                         self.window.connection_window.destroy()
@@ -111,10 +105,8 @@ class Client:
                     if is_valid[0] == "True":
                         bool = False"""
 
-        print("waiting for md5")
         md5 = self.protocol.get_msg(self.client_socket, self.cipher_for_decryption)[2] # should get give md5
         self.target_md5 = str(md5[0])
-        print("got md5: " + str(self.target_md5))
 
     def compute_md5_and_check(self, start, end, target_hash):
         """
@@ -176,7 +168,6 @@ class Client:
 
             data = self.protocol.get_msg(self.client_socket, self.cipher_for_decryption)[2] # should get give range
             start_time = time.time()
-            print(str(data))
             self.range_start = int(data[0])
             self.range_end = int(data[1])
 
